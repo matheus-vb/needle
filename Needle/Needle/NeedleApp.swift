360ed5eece57ceb699a75a6c2f7ddaa8b635d65d
Felipo <gabriel.felipo2010@gmail.com>
2023-05-24 16:35:52 -0300

    Xcode project created

---
+++ Needle/Needle/NeedleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeedleApp.swift
+//  Needle
+//
+//  Created by gabrielfelipo on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct NeedleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
