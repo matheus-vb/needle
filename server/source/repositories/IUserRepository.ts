c01225a3e811c0235703e55269444ab71b0b3ca7
Matheus Batista <matheus.vbatista04@gmail.com>
2023-05-24 18:15:39 -0300

    feature(user-auth): add register user use case

--- server/source/repositories/IUserRepository.ts
+++ server/source/repositories/IUserRepository.ts
@@ -1,6 +1,6 @@
 import { Prisma, User } from "@prisma/client"
 
-interface IUserRepository {
+export interface IUserRepository {
     create(data: Prisma.UserCreateInput): Promise<User>;
     findById(id: string): Promise<User | null>;
     findByEmail(email: string): Promise<User | null>;
