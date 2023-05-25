0dfd222d87ccb20a24189333d2adf6283812c250
Matheus Batista <matheus.vbatista04@gmail.com>
2023-05-25 17:02:45 -0300

    feature(query-documents): fix update documents repo interface

--- server/source/repositories/IDocumentRepository.ts
+++ server/source/repositories/IDocumentRepository.ts
@@ -1,6 +1,6 @@
-import { Document, Prisma } from "@prisma/client";
+import { Document, Prisma, Task } from "@prisma/client";
 
 export interface IDocumentRepository {
     create(data: Prisma.DocumentCreateInput): Promise<Document>;
-    findDocumentByTask(taskId: string): Promise<Document | null>;
+    findDocumentByTask(task: Task): Promise<Document | null>;
 }
