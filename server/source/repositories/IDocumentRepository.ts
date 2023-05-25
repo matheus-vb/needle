92073e8f77936201be41147b6de854ae7eb15e84
Matheus Batista <matheus.vbatista04@gmail.com>
2023-05-25 17:09:07 -0300

    feature(query-documents): add query document to document repo interface

--- server/source/repositories/IDocumentRepository.ts
+++ server/source/repositories/IDocumentRepository.ts
@@ -3,4 +3,5 @@ import { Document, Prisma, Task } from "@prisma/client";
 export interface IDocumentRepository {
     create(data: Prisma.DocumentCreateInput): Promise<Document>;
     findDocumentByTask(task: Task): Promise<Document | null>;
+    queryDocumentByTitle(query: string): Promise<Document[]>;
 }
