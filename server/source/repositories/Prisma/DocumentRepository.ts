90e388ee532ce305915f454d732253003df28883
Matheus Batista <matheus.vbatista04@gmail.com>
2023-05-25 17:03:20 -0300

    feature(query-documents): add find document by task to document repo

--- server/source/repositories/Prisma/DocumentRepository.ts
+++ server/source/repositories/Prisma/DocumentRepository.ts
@@ -1,12 +1,22 @@
-import { Prisma, Document } from "@prisma/client";
+import { Prisma, Document, Task } from "@prisma/client";
 import { prisma } from "../../lib/prisma";
 import { IDocumentRepository } from "../IDocumentRepository";
 
 export class DocumentRepository implements IDocumentRepository {
-    async create(data: Prisma.DocumentCreateInput): Promise<Document> {
+    async create(data: Prisma.DocumentCreateInput) {
         const document = await prisma.document.create({
             data,
         })
+        return document;
+    }
+
+    async findDocumentByTask(task: Task) {
+        const document = await prisma.document.findFirst({
+            where: {
+                task,
+            }
+        })
+
         return document;
     }
 }
