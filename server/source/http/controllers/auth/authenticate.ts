687cf8070bbc539b9ae0e33dad779ff1d304245a
Matheus Batista <matheus.vbatista04@gmail.com>
2023-05-26 16:57:12 -0300

    feature(controllers): add authenticate controller

---
+++ server/source/http/controllers/auth/authenticate.ts
@@ -0,0 +1,25 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { makeAuthenticateUseCase } from "../../../useCases/factories/auth/make-authenticate";
+
+export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
+    const authenticateBodySchema = z.object({
+        email: z.string().email(),
+        password: z.string(),
+    });
+
+    const { email, password } = authenticateBodySchema.parse(request.body);
+
+    try {
+        const authenticateUseCase = makeAuthenticateUseCase();
+
+        const user = await authenticateUseCase.handle({
+            email,
+            password,
+        })
+
+        return reply.status(200).send({ data: user });
+    } catch(err) {
+        return reply.status(400).send({ error: err });
+    }
+}
