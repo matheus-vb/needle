31afec3becaed6959214836c71a30a05480cc30f
Matheus Batista <matheus.vbatista04@gmail.com>
2023-05-26 17:09:26 -0300

    feature(controllers): add query document by tag controller

---
+++ server/source/http/controllers/document/query-document-tag.ts
@@ -0,0 +1,23 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { makeQueryDocumentByTagUseCase } from "../../../useCases/factories/document/make-query-document-tag";
+
+export async function queryDocumentByTaskTag(request: FastifyRequest, reply: FastifyReply) {
+    const queryDocumentByTaskTagBodySchema = z.object({
+        tag: z.string(),
+    })
+
+    const { tag } = queryDocumentByTaskTagBodySchema.parse(request.body);
+
+    try {
+        const queryDocumentByTaskTagUseCase = makeQueryDocumentByTagUseCase();
+
+        const { documents } = await queryDocumentByTaskTagUseCase.handle({
+            tag,
+        })
+
+        return reply.status(200).send({ data: documents });
+    } catch(err) {
+        return reply.status(400).send({ error: err });
+    }
+}
