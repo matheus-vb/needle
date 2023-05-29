e3afbcfe48311851267419dfdbf1ba90a2085a36
João Pedro <jpcm2@cin.ufpe.br>
2023-05-29 16:59:17 -0300

    fix(documentsQuery): fix queryDocumentByType controller

--- server/source/http/controllers/document/query-document-tag.ts
+++ server/source/http/controllers/document/query-document-tag.ts
@@ -5,15 +5,17 @@ import { makeQueryDocumentByTagUseCase } from "../../../useCases/factories/docum
 export async function queryDocumentByTaskTag(request: FastifyRequest, reply: FastifyReply) {
     const queryDocumentByTaskTagBodySchema = z.object({
         tag: z.string(),
+        accessCode: z.string(),
     })
 
-    const { tag } = queryDocumentByTaskTagBodySchema.parse(request.params);
+    const { tag, accessCode } = queryDocumentByTaskTagBodySchema.parse(request.params);
 
     try {
         const queryDocumentByTaskTagUseCase = makeQueryDocumentByTagUseCase();
 
         const { documents } = await queryDocumentByTaskTagUseCase.handle({
             tag,
+            accessCode
         })
 
         return reply.status(200).send({ data: documents });
