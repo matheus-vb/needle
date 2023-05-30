ea43dfe4e528bc5c5f95be2ec4d1f690367c074f
João Pedro <jpcm2@cin.ufpe.br>
2023-05-30 15:04:31 -0300

    feature(login): Init feature

---
+++ Needle/Needle/Core/login/Login.swift
@@ -0,0 +1,20 @@
+//
+//  Login.swift
+//  Needle
+//
+//  Created by jpcm2 on 30/05/23.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        Login()
+    }
+}
---
+++ Needle/Needle/Core/login/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  Needle
+//
+//  Created by jpcm2 on 30/05/23.
+//
+
+import Foundation
