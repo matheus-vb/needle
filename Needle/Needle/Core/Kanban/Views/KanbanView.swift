9864e87135aa52b03fe2591c915425a78cf4047b
João Pedro <jpcm2@cin.ufpe.br>
2023-06-01 16:24:25 -0300

    feature(navHome): merged with develop

--- Needle/Needle/Core/Kanban/Views/KanbanView.swift
+++ Needle/Needle/Core/Kanban/Views/KanbanView.swift
@@ -120,13 +120,3 @@ struct KanbanView: View {
     }
     
 }
-<<<<<<< HEAD
-=======
-
-
-/*struct KanbanView_Previews: PreviewProvider {
-    static var previews: some View {
-        KanbanView(workspace: WorkspaceGridModel(id: "1", accessCode: "123", name: "Projeto Teste", owner: "Felipo"))
-    }
-}*/
->>>>>>> develop
