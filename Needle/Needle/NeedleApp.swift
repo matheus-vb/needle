70cf5066193e36e191bed706ed177e5109fa36e2
Matheus Batista <matheus.vbatista04@gmail.com>
2023-06-01 00:55:10 -0300

    feature(auth-integration): allow http transport to project settings

--- Needle/Needle/NeedleApp.swift
+++ Needle/Needle/NeedleApp.swift
@@ -18,8 +18,10 @@ struct NeedleApp: App {
             //KanbanView(workspace: WorkspaceModel(id: "1", accessCode: "123", name: "Projeto Teste")).frame(minWidth: 900, minHeight: 570)
             
             // Uncomment to start in HomeView
-            DocumentsView()
-                .environmentObject(documentsViewModel)
+//            DocumentsView()
+//                .environmentObject(documentsViewModel)
+            Register()
+                .environmentObject(registerViewModel)
         }
     }
 }
