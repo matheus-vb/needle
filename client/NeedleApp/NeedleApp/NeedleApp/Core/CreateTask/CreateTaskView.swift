249cedb2bd4cecf47731c720fe0ec7398746419f
João Pedro <jpcm2@cin.ufpe.br>
2023-07-26 15:52:05 -0300

    client/feature(CreateTask): Setup

---
+++ client/NeedleApp/NeedleApp/NeedleApp/Core/CreateTask/CreateTaskView.swift
@@ -0,0 +1,23 @@
+//
+//  CreateTaskView.swift
+//  NeedleApp
+//
+//  Created by jpcm2 on 26/07/23.
+//
+
+import SwiftUI
+
+struct CreateTaskView: View {
+    var body: some View {
+        HStack{
+            
+        }
+        .frame(width: 1512, height: 982)
+    }
+}
+
+struct CreateTaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateTaskView()
+    }
+}
