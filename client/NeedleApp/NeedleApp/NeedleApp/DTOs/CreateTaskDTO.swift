598737b1cb0c3d5b39f739dc28f05b139fa91052
João Pedro <jpcm2@cin.ufpe.br>
2023-07-28 15:45:41 -0300

    client/fix(CreateTask): createTask DTO

---
+++ client/NeedleApp/NeedleApp/NeedleApp/DTOs/CreateTaskDTO.swift
@@ -0,0 +1,18 @@
+//
+//  CreateTaskDTO.swift
+//  NeedleApp
+//
+//  Created by jpcm2 on 28/07/23.
+//
+
+import Foundation
+
+struct CreateTaskDTO: Codable{
+    let userID: String
+    let accessCode: String
+    let title: String
+    let description: String
+    let stats: String
+    let type: String
+    let endDate: String
+}
