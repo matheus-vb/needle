fe54337226049602684cde260dc89a027d026ff4
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-03 15:51:55 -0300

    feature(login-integration): create obj to dict converter

--- client/NeedleApp/NeedleApp/NeedleApp/Utilities/ConvertToDict.swift
+++ client/NeedleApp/NeedleApp/NeedleApp/Utilities/ConvertToDict.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+func convertToDictionary<T>(_ obj: T) -> [String: Any] {
+    let mirror = Mirror(reflecting: obj)
+    var dict = [String: Any]()
+    for child in mirror.children {
+        guard let label = child.label else { continue }
+        if let value = child.value as? OptionalProtocol, let unwrapped = value.optional {
+            dict[label] = unwrapped
+        } else {
+            dict[label] = child.value
+        }
+    }
+    return dict
+}
+
+protocol OptionalProtocol {
+    var optional: Any? { get }
+}
+
+extension Optional: OptionalProtocol {
+    var optional: Any? { return self }
+}
