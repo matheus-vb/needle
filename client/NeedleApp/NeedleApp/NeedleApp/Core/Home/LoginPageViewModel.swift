7be44b5f1f6ea89dd22fb119a75a1621f173a90f
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-03 15:52:10 -0300

    feature(login-integration): create login view model

--- client/NeedleApp/NeedleApp/NeedleApp/Core/Home/LoginPageViewModel.swift
+++ client/NeedleApp/NeedleApp/NeedleApp/Core/Home/LoginPageViewModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import Combine
+
+class LoginPageViewModel: ObservableObject {
+    private var authManager = AuthenticationManager.shared
+    private var cancellablels = Set<AnyCancellable>()
+    
+    @Published var user: User?
+    
+    init() {
+        addSubscribers()
+    }
+    
+    func addSubscribers() {
+        authManager.$user
+            .sink(receiveValue: {[weak self] returnedUser in
+                self?.user = returnedUser
+            })
+            .store(in: &cancellablels)
+    }
+}
