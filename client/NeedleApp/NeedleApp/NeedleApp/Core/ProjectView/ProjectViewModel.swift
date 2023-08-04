ebec227d0684518d9d3f502ad079c91cd5c97ce4
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-04 01:07:34 -0300

    feature(tasks-dataservice): update project stub

--- client/NeedleApp/NeedleApp/NeedleApp/Core/ProjectView/ProjectViewModel.swift
+++ client/NeedleApp/NeedleApp/NeedleApp/Core/ProjectView/ProjectViewModel.swift
@@ -9,6 +9,6 @@ import Foundation
 
 class ProjectViewModel: ObservableObject{
     @Published var selectedTab: SelectedTab = .Kanban 
-    @Published var selectedProject: Workspace = Workspace(accessCode: "", name: "")
-    @Published var projects: [Workspace] = [Workspace(accessCode: "Meu codigo 1", name: "Projeto 2"), Workspace(accessCode: "Meu codigo 2", name: "Projeto 1"), Workspace(accessCode: "Meu codigo 3", name: "Projeto 3"), Workspace(accessCode: "Meu codigo 4", name: "Projeto 4")]
+    @Published var selectedProject: Workspace = Workspace(id: "id1", accessCode: "", name: "")
+    @Published var projects: [Workspace] = []
 }
