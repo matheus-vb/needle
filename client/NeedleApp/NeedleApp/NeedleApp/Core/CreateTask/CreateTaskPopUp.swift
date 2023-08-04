74b1748796d51605cb37a2113affc1a71146af94
João Pedro <jpcm2@cin.ufpe.br>
2023-08-04 15:23:35 -0300

    client/fix(CreateTask): attributes label

--- client/NeedleApp/NeedleApp/NeedleApp/Core/CreateTask/CreateTaskPopUp.swift
+++ client/NeedleApp/NeedleApp/NeedleApp/Core/CreateTask/CreateTaskPopUp.swift
@@ -11,8 +11,14 @@ struct CreateTaskPopUp: View {
     @StateObject var createTaskViewModel: CreateTaskViewModel = CreateTaskViewModel()
     var geometry: GeometryProxy
     var body: some View {
-        VStack{
-           taskTitle
+        VStack(spacing: 30){
+            taskTitle
+            VStack(alignment: .leading){
+                deadLineLabel
+                responsibleLabel
+                typeLabel
+                priorityLabel
+            }
         }
         .frame(width: geometry.size.width/3.0, height: geometry.size.height)
         .background(.white)
@@ -22,5 +28,47 @@ struct CreateTaskPopUp: View {
 extension CreateTaskPopUp{
     var taskTitle: some View {
         TitleEditableText(text: $createTaskViewModel.taskTitle)
+    }
+    
+    var deadLineLabel: some View {
+        HStack{
+            Image(systemName: "calendar")
+                .resizable()
+                .frame(width: 20, height: 20)
+            Text("Prazo")
+        }
+        .font(.system(size: 16))
+        .foregroundColor(Color.theme.mainGray)
+    }
+    
+    var responsibleLabel: some View{
+        HStack{
+            Image(systemName: "person.fill")
+                .resizable()
+                .frame(width: 20, height: 20)
+            Text("Responsável")
+        }
+        .font(.system(size: 16))
+        .foregroundColor(Color.theme.mainGray)
+    }
+    var typeLabel: some View{
+        HStack{
+            Image(systemName: "shippingbox")
+                .resizable()
+                .frame(width: 20, height: 20)
+            Text("Área")
+        }
+        .font(.system(size: 16))
+        .foregroundColor(Color.theme.mainGray)
+    }
+    var priorityLabel: some View{
+        HStack{
+            Image(systemName: "flag.fill")
+                .resizable()
+                .frame(width: 20, height: 20)
+            Text("Prioridade")
+        }
+        .font(.system(size: 16))
+        .foregroundColor(Color.theme.mainGray)
     }
 }
