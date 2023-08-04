450af915a6787714eae00b066d603c725e938deb
João Pedro <jpcm2@cin.ufpe.br>
2023-08-04 15:51:47 -0300

    client/fix(CreateTask): attributes done

---
+++ client/NeedleApp/NeedleApp/NeedleApp/Core/CreateTask/Components/CreateTaskPopUPComponents.swift
@@ -0,0 +1,69 @@
+//
+//  CreateTaskPopUPComponents.swift
+//  NeedleApp
+//
+//  Created by jpcm2 on 04/08/23.
+//
+
+import SwiftUI
+
+
+extension CreateTaskPopUp{
+    var taskTitle: some View {
+        TitleEditableText(text: $createTaskViewModel.taskTitle)
+    }
+    var deadLine: some View{
+        HStack(spacing: 24){
+            LabelComponent(imageName: "calendar", label: "Prazo")
+            DatePicker(selection: $createTaskViewModel.deadLineSelection, in: Date.now..., displayedComponents: .date) {
+                Text("Select a date")
+            }
+            .colorInvert()
+            .colorMultiply(Color.theme.mainBlack)
+            .labelsHidden()
+            .background(Color.clear)
+            .border(Color.clear)
+        }
+        .font(.system(size: 16))
+        .foregroundColor(Color.theme.mainGray)
+    }
+    
+    var responsible: some View {
+        HStack(spacing: 24){
+            LabelComponent(imageName: "person.fill", label: "Responsável")
+            Picker("Área",selection: $createTaskViewModel.selectedMember){
+                ForEach(createTaskViewModel.members, id: \.self) {membro in
+                    Text(membro.name)
+                }
+            }
+            .pickerStyle(.menu)
+            .labelsHidden()
+        }
+    }
+        
+    var type: some View {
+        HStack(spacing: 24){
+            LabelComponent(imageName: "shippingbox", label:"Área")
+            Picker("Área",selection: $createTaskViewModel.categorySelection){
+               ForEach(TaskType.allCases, id: \.self) { type in
+                   Text(type.rawValue)
+               }
+           }
+           .pickerStyle(.menu)
+           .labelsHidden()
+        }
+    }
+    
+    var priority: some View {
+        HStack(spacing: 24){
+            LabelComponent(imageName: "flag.fill", label: "Prioridade")
+            Picker("Prioridade",selection: $createTaskViewModel.prioritySelection){
+                ForEach(TaskPriority.allCases, id: \.self) { priority in
+                    Text(priority.rawValue)
+                }
+            }
+            .pickerStyle(.menu)
+            .labelsHidden()
+        }
+    }
+}
--- client/NeedleApp/NeedleApp/NeedleApp/Core/CreateTask/CreateTaskPopUp.swift
+++ client/NeedleApp/NeedleApp/NeedleApp/Core/CreateTask/CreateTaskPopUp.swift
@@ -14,61 +14,13 @@ struct CreateTaskPopUp: View {
         VStack(spacing: 30){
             taskTitle
             VStack(alignment: .leading){
-                deadLineLabel
-                responsibleLabel
-                typeLabel
-                priorityLabel
+                deadLine
+                responsible
+                type
+                priority
             }
         }
         .frame(width: geometry.size.width/3.0, height: geometry.size.height)
         .background(.white)
-    }
-}
-
-extension CreateTaskPopUp{
-    var taskTitle: some View {
-        TitleEditableText(text: $createTaskViewModel.taskTitle)
-    }
-    
-    var deadLineLabel: some View {
-        HStack{
-            Image(systemName: "calendar")
-                .resizable()
-                .frame(width: 20, height: 20)
-            Text("Prazo")
-        }
-        .font(.system(size: 16))
-        .foregroundColor(Color.theme.mainGray)
-    }
-    
-    var responsibleLabel: some View{
-        HStack{
-            Image(systemName: "person.fill")
-                .resizable()
-                .frame(width: 20, height: 20)
-            Text("Responsável")
-        }
-        .font(.system(size: 16))
-        .foregroundColor(Color.theme.mainGray)
-    }
-    var typeLabel: some View{
-        HStack{
-            Image(systemName: "shippingbox")
-                .resizable()
-                .frame(width: 20, height: 20)
-            Text("Área")
-        }
-        .font(.system(size: 16))
-        .foregroundColor(Color.theme.mainGray)
-    }
-    var priorityLabel: some View{
-        HStack{
-            Image(systemName: "flag.fill")
-                .resizable()
-                .frame(width: 20, height: 20)
-            Text("Prioridade")
-        }
-        .font(.system(size: 16))
-        .foregroundColor(Color.theme.mainGray)
     }
 }
