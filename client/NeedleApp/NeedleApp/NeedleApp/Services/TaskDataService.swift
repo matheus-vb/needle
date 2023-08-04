2b66e69ad8d474239f10b53b33aabde1c1d884ca
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-04 01:06:05 -0300

    feature(tasks-dataservice): create task data service

--- client/NeedleApp/NeedleApp/NeedleApp/Services/TaskDataService.swift
+++ client/NeedleApp/NeedleApp/NeedleApp/Services/TaskDataService.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import Combine
+
+class TaskDataService: ObservableObject {
+    static let shared = TaskDataService()
+    private init() {}
+    
+    @Published var currError: NetworkingManager.NetworkingError?
+    @Published var errorCount: Int = 0
+    
+    @Published var allUsersTasks: [String: [TaskModel]] = [:]
+    
+    var getWorkspaceTasksSubscription: AnyCancellable?
+    var createTaskSubscription: AnyCancellable?
+    
+    func getWorkspaceTasks(userId: String, workspaceId: String) {
+        guard let url = URL(string: Bundle.baseURL + "task/\(workspaceId)") else { return }
+        
+        getWorkspaceTasksSubscription = NetworkingManager.download(url: url)
+            .decode(type: TaskResponse.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: {
+                completion in NetworkingManager.handleCompletion(completion: completion) { error in
+                    self.currError = error as? NetworkingManager.NetworkingError
+                    self.errorCount += 1
+                }
+            }, receiveValue: { [weak self] (returnedTasks) in
+                self?.allUsersTasks[workspaceId] = returnedTasks.data
+                self?.getWorkspaceTasksSubscription?.cancel()
+            })
+    }
+    
+    func createTask(dto: CreateTaskDTO, userId: String, workspaceId: String) {
+        guard let url = URL(string: Bundle.baseURL + "task/create") else { return }
+        
+        let parameters = convertToDictionary(dto)
+        guard let jsonData = try? JSONSerialization.data(withJSONObject: parameters) else { return }
+        
+        createTaskSubscription = NetworkingManager.post(url: url, body: jsonData)
+            .sink(receiveCompletion: {
+                completion in NetworkingManager.handleCompletion(completion: completion) { error in
+                    self.currError = error as? NetworkingManager.NetworkingError
+                    self.errorCount += 1
+                }
+            }, receiveValue: { [weak self] _ in
+                self?.getWorkspaceTasks(userId: userId, workspaceId: workspaceId)
+                self?.createTaskSubscription?.cancel()
+            })
+    }
+}
