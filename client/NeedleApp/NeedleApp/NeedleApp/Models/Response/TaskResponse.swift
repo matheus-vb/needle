22e7cdc73aadd80edb6bcc9e3aa5c3a5c363254a
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-04 01:01:24 -0300

    feature(tasks-dataservice): update project

---
+++ client/NeedleApp/NeedleApp/NeedleApp/Models/Response/TaskResponse.swift
@@ -0,0 +1,8 @@
+//
+//  TaskResponse.swift
+//  NeedleApp
+//
+//  Created by matheusvb on 04/08/23.
+//
+
+import Foundation
---
+++ client/NeedleApp/NeedleApp/NeedleApp/Services/TaskDataService.swift
@@ -0,0 +1,8 @@
+//
+//  TaskDataService.swift
+//  NeedleApp
+//
+//  Created by matheusvb on 04/08/23.
+//
+
+import Foundation
