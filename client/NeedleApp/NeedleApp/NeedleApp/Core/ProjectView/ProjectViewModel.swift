004fe2acfd433c69aa9c447bb29606c83de15311
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-04 23:45:42 -0300

    feature(kanban-integration): add tasks to project view model

--- client/NeedleApp/NeedleApp/NeedleApp/Core/ProjectView/ProjectViewModel.swift
+++ client/NeedleApp/NeedleApp/NeedleApp/Core/ProjectView/ProjectViewModel.swift
@@ -12,10 +12,12 @@ class ProjectViewModel: ObservableObject{
     @Published var selectedTab: SelectedTab = .Kanban 
     @Published var selectedProject: Workspace = Workspace(id: "id1", accessCode: "", name: "")
     @Published var projects: [Workspace] = []
+    @Published var tasks: [String:[TaskModel]] = [:]
     
-    @Published var triggerLoading = false
+    @Published var triggerLoading = true
     
     private var worskpaceDS = WorkspaceDataService.shared
+    private var tasksDS = TaskDataService.shared
     private var cancellables = Set<AnyCancellable>()
     
     init() {
@@ -26,6 +28,12 @@ class ProjectViewModel: ObservableObject{
         worskpaceDS.$workspaces
             .sink(receiveValue: { [weak self] returnedWorkspaces in
                 self?.projects = returnedWorkspaces
+            })
+            .store(in: &cancellables)
+        
+        tasksDS.$allUsersTasks
+            .sink(receiveValue: { [weak self] returnedTasks in
+                self?.tasks = returnedTasks
             })
             .store(in: &cancellables)
     }
