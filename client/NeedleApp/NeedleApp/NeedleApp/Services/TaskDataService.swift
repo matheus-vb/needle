ccbfaf65bac1fc9825686ca818877cf97e5f6928
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-04 23:43:24 -0300

    feature(kanban-integration): add update task status to data service

--- client/NeedleApp/NeedleApp/NeedleApp/Services/TaskDataService.swift
+++ client/NeedleApp/NeedleApp/NeedleApp/Services/TaskDataService.swift
@@ -20,6 +20,7 @@ class TaskDataService: ObservableObject {
     var getWorkspaceTasksSubscription: AnyCancellable?
     var getAllUsersTasksSubscription: AnyCancellable?
     var createTaskSubscription: AnyCancellable?
+    var updateTaskStatusSubscription: AnyCancellable?
     
     func getWorkspaceTasks(userId: String, workspaceId: String) {
         guard let url = URL(string: Bundle.baseURL + "task/\(workspaceId)") else { return }
@@ -44,6 +45,26 @@ class TaskDataService: ObservableObject {
         guard let jsonData = try? JSONSerialization.data(withJSONObject: parameters) else { return }
         
         createTaskSubscription = NetworkingManager.post(url: url, body: jsonData)
+            .sink(receiveCompletion: {
+                completion in NetworkingManager.handleCompletion(completion: completion) { error in
+                    self.currError = error as? NetworkingManager.NetworkingError
+                    self.errorCount += 1
+                }
+            }, receiveValue: { [weak self] _ in
+                self?.getWorkspaceTasks(userId: userId, workspaceId: workspaceId)
+                self?.createTaskSubscription?.cancel()
+            })
+    }
+    
+    func updateTaskStatus(taskId: String, status: TaskStatus, userId: String, workspaceId: String) {
+        guard let url = URL(string: Bundle.baseURL + "task") else { return }
+        
+        let dto = UpdateTaskStatusDTO(taskId: taskId, status: status.rawValue)
+        
+        let parameters = convertToDictionary(dto)
+        guard let jsonData = try? JSONSerialization.data(withJSONObject: parameters) else { return }
+        
+        updateTaskStatusSubscription = NetworkingManager.patch(url: url, body: jsonData)
             .sink(receiveCompletion: {
                 completion in NetworkingManager.handleCompletion(completion: completion) { error in
                     self.currError = error as? NetworkingManager.NetworkingError
