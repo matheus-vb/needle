0343affbc8ed978b09743fd4bb5ee71cc70a75d7
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-08 23:50:16 -0300

    feature(get-role): add get role to app

--- server/source/http/controllers/user/routes.ts
+++ server/source/http/controllers/user/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { updateDeviceToken } from "./update-device";
+import { getRole } from "./get-role";
 
 export async function userRoutes(app: FastifyInstance) {
     app.patch('/user/device', updateDeviceToken);
+    app.get('/user/:workspaceId/:userId', getRole);
 }
