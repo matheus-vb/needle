ccf7714bfc36723117cad46fb2d82737efbc2829
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-09 15:19:45 -0300

    fix(toolbar): update project

---
+++ client/NeedleApp/NeedleApp/NeedleApp/Core/Root/AppView.swift
@@ -0,0 +1,20 @@
+//
+//  AppView.swift
+//  NeedleApp
+//
+//  Created by matheusvb on 09/08/23.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
