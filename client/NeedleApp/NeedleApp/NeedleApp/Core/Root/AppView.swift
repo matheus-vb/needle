6ad7d987d8cd2ab71129e6f9ff303bd09e1b2815
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-09 15:20:48 -0300

    fix(toolbar): move app view to run toolbar

--- client/NeedleApp/NeedleApp/NeedleApp/Core/Root/AppView.swift
+++ client/NeedleApp/NeedleApp/NeedleApp/Core/Root/AppView.swift
@@ -9,12 +9,8 @@ import SwiftUI
 
 struct AppView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct AppView_Previews: PreviewProvider {
-    static var previews: some View {
-        AppView()
+        NavigationStack {
+            WorkspaceHomeView()            
+        }
     }
 }
