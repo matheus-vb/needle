a3b8a8d0e4f62aa73d2d01b53dbe8e8cfc45061e
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-08 22:10:14 -0300

    feature(search-docsVM): update project

---
+++ client/NeedleApp/NeedleApp/NeedleApp/Core/SearchDocuments/SearchDocumentsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchDocumentsViewModel.swift
+//  NeedleApp
+//
+//  Created by matheusvb on 08/08/23.
+//
+
+import Foundation
---
+++ client/NeedleApp/NeedleApp/NeedleApp/DTOs/QueryTasksDTO.swift
@@ -0,0 +1,8 @@
+//
+//  QueryTasksDTO.swift
+//  NeedleApp
+//
+//  Created by matheusvb on 08/08/23.
+//
+
+import Foundation
