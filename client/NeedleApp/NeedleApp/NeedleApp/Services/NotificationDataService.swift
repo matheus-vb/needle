f789c99169f4d0c3b4d8501fa2a6b3559fedba61
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-10 15:28:07 -0300

    feature(notifications): add deletion to not data service

--- client/NeedleApp/NeedleApp/NeedleApp/Services/NotificationDataService.swift
+++ client/NeedleApp/NeedleApp/NeedleApp/Services/NotificationDataService.swift
@@ -16,9 +16,13 @@ class NotificationDataService: ObservableObject {
     var token: String?
     
     var updateDeviceTokenSubscription: AnyCancellable?
+    var getUserNotificationSubscription: AnyCancellable?
+    var deleteUserNotificationSubscription: AnyCancellable?
     
     @Published var currError: NetworkingManager.NetworkingError?
     @Published var errorCount: Int = 0
+    
+    @Published var usersNotifications: [NotificationModel] = []
     
     func updateDeviceToken(userId: String) {
         guard let url = URL(string: Bundle.baseURL + "user/device") else { return }
@@ -38,6 +42,37 @@ class NotificationDataService: ObservableObject {
                 }
             }, receiveValue: { [weak self] _ in
                 self?.updateDeviceTokenSubscription?.cancel()
+            })
+    }
+    
+    func getUserNotifications(userId: String) {
+        guard let url = URL(string: Bundle.baseURL + "notification/\(userId)") else { return }
+        
+        getUserNotificationSubscription = NetworkingManager.download(url: url)
+            .decode(type: NotificationResponse.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: {
+                completion in NetworkingManager.handleCompletion(completion: completion) { error in
+                    self.currError = error as? NetworkingManager.NetworkingError
+                    self.errorCount += 1
+                }
+            }, receiveValue: { [weak self] (returnedNotifications) in
+                self?.usersNotifications = returnedNotifications.data
+                self?.getUserNotificationSubscription?.cancel()
+            })
+    }
+    
+    func deleteUserNotifications(userId: String) {
+        guard let url = URL(string: Bundle.baseURL + "notification/\(userId)") else { return }
+        
+        deleteUserNotificationSubscription = NetworkingManager.delete(url: url)
+            .sink(receiveCompletion: {
+                completion in NetworkingManager.handleCompletion(completion: completion) { error in
+                    self.currError = error as? NetworkingManager.NetworkingError
+                    self.errorCount += 1
+                }
+            }, receiveValue: { [weak self] _ in
+                self?.getUserNotifications(userId: userId)
+                self?.deleteUserNotificationSubscription?.cancel()
             })
     }
 }
