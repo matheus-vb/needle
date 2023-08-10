08887b7b4e4939e83ee9d7265ee881bec5e4f40e
João Pedro <jpcm2@cin.ufpe.br>
2023-08-10 09:21:42 -0300

    client/feature(deleteTask): delete task controller

---
+++ server/source/http/controllers/task/delete-task.ts
@@ -0,0 +1,21 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { makeDeleteTask } from "../../../useCases/factories/task/make-delete-task";
+
+export async function deleteTask(request: FastifyRequest, response: FastifyReply){
+    const deleteTaskBodySchema = z.object({
+        taskId: z.string()
+    })
+
+    const { taskId } = deleteTaskBodySchema.parse(request.body)
+
+    try{
+        const deleteTaskUseCase = makeDeleteTask();
+
+        deleteTaskUseCase.handle({ taskId })
+
+        return response.status(204);
+    }catch(e){
+        throw(e)
+    }
+}
