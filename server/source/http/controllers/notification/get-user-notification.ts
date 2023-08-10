1f194b6d158b7ef58f6607d3877699d7819ddc1f
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-10 15:27:40 -0300

    feature(notifications): add deletion to server

---
+++ server/source/http/controllers/notification/get-user-notification.ts
@@ -0,0 +1,23 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { makeGetUserNotifications } from "../../../useCases/factories/notification/make-get-user-notifications";
+
+export async function getUserNotifications(request: FastifyRequest, reply: FastifyReply) {
+    const getUserNotificationsParamsSchema = z.object({
+        userId: z.string()
+    })
+
+    const { userId } = getUserNotificationsParamsSchema.parse(request.params);
+
+    try {
+        const getUserNotificationsUseCase = makeGetUserNotifications()
+
+        const { notifications } = await getUserNotificationsUseCase.handle({
+            userId,
+        })
+
+        return reply.status(200).send({ data: notifications })
+    } catch (err) {
+        return reply.status(400).send({ err: err })
+    }
+}
