ff7708903448980de8a87abcc565decd0ee1f203
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-09 21:27:21 -0300

    feature(search): create operator overload

--- client/NeedleApp/NeedleApp/NeedleApp/Utilities/OperatorOverload.swift
+++ client/NeedleApp/NeedleApp/NeedleApp/Utilities/OperatorOverload.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import SwiftUI
+
+func ??<T>(lhs: Binding<Optional<T>>, rhs: T) -> Binding<T> {
+    Binding(
+        get: { lhs.wrappedValue ?? rhs },
+        set: { lhs.wrappedValue = $0 }
+    )
+}
