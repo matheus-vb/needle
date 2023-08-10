eb012890e9f639d2ba294cd299f6702fe0d5bb33
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-10 11:07:24 -0300

    feature(apn-singleton): refactor provider into singleton

--- server/source/notification/provider.ts
+++ server/source/notification/provider.ts
@@ -1,14 +1,30 @@
 import { env } from "../env";
 import apn from "apn"
 
-let options: apn.ProviderOptions = {
-    token: {
-        key: "./auth-key.p8",
-        keyId: env.KEY_ID,
-        teamId: env.TEAM_ID,
-    },
-    production: env.NODE_ENV === "PROD" ? true : false
+export class APNSingleton {
+    private static instance: APNSingleton
+
+    public provider: apn.Provider
+
+    private constructor() {
+        let options: apn.ProviderOptions = {
+            token: {
+                key: "./auth-key.p8",
+                keyId: env.KEY_ID,
+                teamId: env.TEAM_ID,
+            },
+            production: env.NODE_ENV === "PROD" ? true : false
+        }
+
+        this.provider = new apn.Provider(options)
+    }
+
+
+    public static shared(): APNSingleton {
+        if (!APNSingleton.instance) {
+            APNSingleton.instance = new APNSingleton();
+        }
+
+        return APNSingleton.instance
+    }
 }
-
-
-export const apnProvider = new apn.Provider(options);
