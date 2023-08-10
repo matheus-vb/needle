c2a0655ee027b937dff51312bcc8071b165254a5
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-10 14:20:37 -0300

    feature(not-model): create separate apns push function

---
+++ server/source/notification/push-apns.ts
@@ -0,0 +1,25 @@
+import { env } from "../env";
+import { APNSingleton } from "./provider";
+import apn from "apn"
+
+export function pushAPNS(deviceToken: string, alert: string) {
+    if(env.NODE_ENV === "DEV") {
+        return
+    }
+    
+    const provider = APNSingleton.shared().provider
+
+    var notification = new apn.Notification();
+
+    notification.topic = env.BUNDLE_ID
+    notification.expiry = Math.floor(Date.now() / 1000) + 3600;
+    notification.badge = 3;
+    notification.sound = "default";
+    notification.alert = alert;
+    notification.payload = {'messageFrom': 'Needle App'};
+
+    provider.send(notification, deviceToken).then((result) => {
+        console.log(result.failed)
+        console.log(result.sent)
+    })
+}
