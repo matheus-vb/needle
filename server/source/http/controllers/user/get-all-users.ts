941b2d773e80a07f5bcd4c36138d4a58a7f5ea17
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-10 23:03:49 -0300

    feature(get-users): create get all users controller

---
+++ server/source/http/controllers/user/get-all-users.ts
@@ -0,0 +1,14 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { makeGetAllUsers } from "../../../useCases/factories/user/make-get-all-users";
+
+export async function getAllUsers(request: FastifyRequest, reply: FastifyReply) {
+    try {
+        const getAllUsersUseCase = makeGetAllUsers()
+
+        const { users } = await getAllUsersUseCase.handle()
+
+        return reply.status(200).send({ data: users })
+    } catch(err) {
+        return reply.status(400).send({ err: err })
+    }
+}
--- server/source/http/controllers/user/routes.ts
+++ server/source/http/controllers/user/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { updateDeviceToken } from "./update-device";
 import { getRole } from "./get-role";
+import { getAllUsers } from "./get-all-users";
 
 export async function userRoutes(app: FastifyInstance) {
     app.patch('/user/device', updateDeviceToken);
     app.get('/user/:workspaceId/:userId', getRole);
+    app.get('/user', getAllUsers);
 }
