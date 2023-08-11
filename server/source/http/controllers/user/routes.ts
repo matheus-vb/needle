603464dda4307c1b379d9525faa46e0b95926883
João Pedro <jpcm2@cin.ufpe.br>
2023-08-10 23:25:04 -0300

    server/feature(deleteUser): route added

--- server/source/http/controllers/user/routes.ts
+++ server/source/http/controllers/user/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { updateDeviceToken } from "./update-device";
 import { getRole } from "./get-role";
+import { deleteUser } from "./delete-user";
 
 export async function userRoutes(app: FastifyInstance) {
     app.patch('/user/device', updateDeviceToken);
     app.get('/user/:workspaceId/:userId', getRole);
+    app.delete('/user/delete/:userId', deleteUser)
 }
