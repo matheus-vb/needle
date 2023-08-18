abb10cf4c10c861d43a2c4575e4b3b70864f5cc7
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-18 19:08:48 -0300

    chore(protocols): create task protocol

--- client/NeedleApp/NeedleApp/NeedleApp/Protocols/TaskDataServiceProtocol.swift
+++ client/NeedleApp/NeedleApp/NeedleApp/Protocols/TaskDataServiceProtocol.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+protocol TaskDataServiceProtocol: ObservableObject {
+    func getWorkspaceTasks(userId: String, workspaceId: String)
+    func createTask(dto: CreateTaskDTO, userId: String, workspaceId: String)
+    func updateTaskStatus(taskId: String, status: TaskStatus, userId: String, workspaceId: String)
+    func queryTasks(dto: QueryTasksDTO)
+    func saveTask(dto: SaveTaskDTO, userId: String, workspaceId: String)
+    func deleteTask(dto: DeleteTaskDTO, userId: String, workspaceId: String)
+}
