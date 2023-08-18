e14d844d15e7a2d6a64413441b23782e1f491e61
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-18 18:47:44 -0300

    chore(root-refactor): create root view model draft

--- client/NeedleApp/NeedleApp/NeedleApp/Core/Root/RootViewModel.swift
+++ client/NeedleApp/NeedleApp/NeedleApp/Core/Root/RootViewModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import SwiftUI
+
+class RootViewModel: ObservableObject {
+    @ObservedObject var authManager = AuthenticationManager.shared
+    @Published var notificationIsPresented : Bool = false
+    @Published var userLogoutIsPresented : Bool = false
+    @Published var showErrorSheet: Bool = false
+    @ObservedObject var notificationViewModel = NotificationBarViewModel()
+    
+    func presentNotifications() {
+        NotificationDataService.shared.getUserNotifications(userId: AuthenticationManager.shared.user!.id)
+        self.notificationIsPresented.toggle()
+    }
+    
+    func logout() {
+        self.authManager.user = nil
+    }
+}
