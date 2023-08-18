573b5e7d6f40e8224b04f0f96164ca4bc49eaea9
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-18 19:09:05 -0300

    chore(protocols): create workspace protocol

--- client/NeedleApp/NeedleApp/NeedleApp/Protocols/WorkspaceDataServiceProtocol.swift
+++ client/NeedleApp/NeedleApp/NeedleApp/Protocols/WorkspaceDataServiceProtocol.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+protocol WorkspaceDataServiceProtocol: ObservableObject {
+    func getUsersWorkspaces(userId: String)
+    func createWorkspace(userId: String, name: String)
+    func joinWorkspace(userId: String, accessCode: String, role: Role)
+    func deleteWorkspace(accessCode: String, userId: String)
+    func getWorkspaceMembers(workspaceId: String)
+}
