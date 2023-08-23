4464e70cfc345633f4bffa6af28ff56be14f5150
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-23 14:50:50 -0300

    chore(create-refactor): pass params to create task initializer

--- client/NeedleApp/NeedleApp/NeedleApp/Core/ProjectView/ProjectView.swift
+++ client/NeedleApp/NeedleApp/NeedleApp/Core/ProjectView/ProjectView.swift
@@ -87,7 +87,7 @@ struct ProjectView: View {
                     .environmentObject(EditTaskViewModel(data: projectViewModel.selectedTask!, workspaceID: projectViewModel.selectedWorkspace.id, members: projectViewModel.workspaceMembers[projectViewModel.selectedWorkspace.id] ?? [], isEditing: $projectViewModel.showEditTaskPopUP))
             })
             .sheet(isPresented: $projectViewModel.showPopUp, content: {
-                CreateTaskPopUp(createTaskViewModel: CreateTaskViewModel(members: projectViewModel.workspaceMembers[projectViewModel.selectedWorkspace.id] ?? []), geometry: geometry)
+                CreateTaskPopUp(createTaskViewModel: CreateTaskViewModel(members: projectViewModel.workspaceMembers[projectViewModel.selectedWorkspace.id] ?? [], showPopUp: $projectViewModel.showPopUp, selectedWorkspace: projectViewModel.selectedWorkspace, selectedStatus: projectViewModel.selectedColumnStatus), geometry: geometry)
                     .environmentObject(projectViewModel)
             })
             .onAppear{
