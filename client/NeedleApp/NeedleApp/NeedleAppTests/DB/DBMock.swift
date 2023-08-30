33e72f0afa28a916cd0f4ab20177bd2ceaafb7c8
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-30 16:14:31 -0300

    chore(rootVM-tests): create db mock class

--- client/NeedleApp/NeedleApp/NeedleAppTests/DB/DBMock.swift
+++ client/NeedleApp/NeedleApp/NeedleAppTests/DB/DBMock.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+class DBMock {
+    var users: [User] = []
+
+    var workspaces: [String: [Workspace]] = [:]
+    var usersInWorkspace: [String: [User]] = [:]
+    var tasksInWorkspace: [String: [TaskModel]] = [:]
+    var notifications: [String: [NotificationModel]] = [:]
+    var roles: [String: Role] = [:]
+    var deviceToken: [String: String] = [:]
+}
