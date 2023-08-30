5345d8f1bb530114ebe9d688b86c7ede9363e0f1
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-30 16:14:45 -0300

    chore(rootVM-tests): create auth manager mock

--- client/NeedleApp/NeedleApp/NeedleAppTests/Services/AuthenticationManagerMock.swift
+++ client/NeedleApp/NeedleApp/NeedleAppTests/Services/AuthenticationManagerMock.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import Combine
+
+class AuthenticationManagerMock: AuthenticationManagerProtocol {
+    var currError: NetworkingManager.NetworkingError?
+    
+    let db: DBMock
+    
+    init (db: DBMock) {
+        self.db = db
+    }
+    
+    @Published var user: User?
+    @Published var roles: [String: Role] = [:]
+    var rolesPublihser: Published<[String: Role]>.Publisher { $roles }
+    
+    func singIn(userId: String, email: String?, name: String?) {
+        guard let user = db.users.first(where: { $0.id == userId }) else {
+            guard let email, let name else { return } //TODO: add error
+            
+            let currUser = User(id: userId, name: name, email: email, workspaces: [])
+            
+            db.users.append(currUser)
+            self.user = currUser
+            
+            return
+        }
+        
+        self.user = user
+    }
+    
+    func getRoleInWorkspace(userId: String, workspaceId: String) {
+        guard let roleInWorkspace = db.roles[workspaceId] else { return } //TODO: add error
+        
+        self.roles[workspaceId] = roleInWorkspace
+    }
+}
