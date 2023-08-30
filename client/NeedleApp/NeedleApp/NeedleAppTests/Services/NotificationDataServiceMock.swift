a2f27eb4c9522cb7ddde9709f54664c0235c2303
Matheus Batista <matheus.vbatista04@gmail.com>
2023-08-30 16:14:58 -0300

    chore(rootVM-tests): create notification ds mock

--- client/NeedleApp/NeedleApp/NeedleAppTests/Services/NotificationDataServiceMock.swift
+++ client/NeedleApp/NeedleApp/NeedleAppTests/Services/NotificationDataServiceMock.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import Combine
+
+class NotificationDataServiceMock: NotificationDataServiceProtocol {
+    var currError: NetworkingManager.NetworkingError?
+    
+    let db: DBMock
+    
+    init(db: DBMock) {
+        self.db = db
+    }
+    
+    var token: String?
+    @Published var usersNotifications: [NotificationModel] = []
+    var usersNotificationsPublisher: Published<[NotificationModel]>.Publisher { $usersNotifications }
+    
+    func updateDeviceToken(userId: String) {
+        guard let token else { return } //TODO: add error
+        
+        self.db.deviceToken[userId] = token
+    }
+    
+    func getUserNotifications(userId: String) {
+        guard let notifications = self.db.notifications[userId] else { return } //TODO: add error
+        
+        self.usersNotifications = notifications
+    }
+    
+    func deleteUserNotifications(userId: String) {
+        self.db.notifications[userId] = []
+    }
+}
