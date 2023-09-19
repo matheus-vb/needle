95b5fc39de949f96207e95cf0b3a53c00bb47a65
Beatriz Férre <93141694+biaferre@users.noreply.github.com>
2023-09-19 15:36:26 -0300

    fix(ProjectView): rm kanban title from project and inserted into kanbanview

--- client/NeedleApp/NeedleApp/NeedleApp/Core/ProjectView/ProjectsViewRightSide.swift
+++ client/NeedleApp/NeedleApp/NeedleApp/Core/ProjectView/ProjectsViewRightSide.swift
@@ -13,16 +13,6 @@ extension ProjectView {
             topContainer
                 .padding([.top], 64)
                 .padding([.leading, .trailing], 64)
-            HStack {
-                statusTitleLabel(rowName: NSLocalizedString("A fazer", comment: ""), color: Color.theme.redMain)
-                statusTitleLabel(rowName: NSLocalizedString("Fazendo", comment: ""), color: Color.theme.blueKanban)
-                statusTitleLabel(rowName: NSLocalizedString("Em revisão", comment: ""), color: Color.theme.orangeKanban)
-                statusTitleLabel(rowName: NSLocalizedString("Feito", comment: ""), color: Color.theme.greenKanban)
-            }
-            .padding(.leading, 64)
-            .padding(.trailing, 64)
-            .padding(.top, 24)
-            .offset(x: 12)
             
             if projectViewModel.selectedTab == .Kanban{
                 KanbanView(tasks: projectViewModel.tasks[projectViewModel.selectedWorkspace.id] ?? [], role: projectViewModel.roles[projectViewModel.selectedWorkspace.id] ?? .DEVELOPER, selectedColumn: $projectViewModel.selectedColumnStatus, showPopUp: $projectViewModel.showPopUp, showCard: $projectViewModel.showCard, selectedWorkspace: projectViewModel.selectedWorkspace, selectedTask: $projectViewModel.selectedTask, isEditing: $projectViewModel.showEditTaskPopUP)
