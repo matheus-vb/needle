934604b3cb6f0e2cc72eae4394c2a4b9b48eebfc
Matheus Batista <matheus.vbatista04@gmail.com>
2023-09-26 15:34:50 -0300

    feature(update-role): implement update role in prisma repo

--- server/source/repositories/Prisma/UserWorkspaceRepository.ts
+++ server/source/repositories/Prisma/UserWorkspaceRepository.ts
@@ -1,6 +1,7 @@
-import { Prisma, User_Workspace } from "@prisma/client";
+import { Prisma, Role, User_Workspace } from "@prisma/client";
 import { prisma } from "../../lib/prisma";
 import { IUserWorkspaceRepository } from "../IUserWorkspaceRepository";
+import { truncate } from "fs";
 
 
 export class UserWorkspaceRepository implements IUserWorkspaceRepository{
@@ -58,5 +59,19 @@ export class UserWorkspaceRepository implements IUserWorkspaceRepository{
             return false;
         }
         return true;
+    }
+
+    async updateRole(userId: string, workspaceId: string, role: Role) {
+        await prisma.user_Workspace.updateMany({
+            where: {
+                userId: userId,
+                workspaceId: workspaceId
+            },
+            data:{
+                userRole: role,
+            }
+        })
+
+        return true
     }
 }
