0922ec2277984530b87cf7dbdd18974154dca470
Beatriz Férre <93141694+biaferre@users.noreply.github.com>
2023-10-05 14:22:28 -0300

    create documentation view

---
+++ client/NeedleApp/NeedleApp/NeedleApp/Core/CreateDocumentation/CreateDocumentationView.swift
@@ -0,0 +1,17 @@
+//
+//  CreateDocumentationView.swift
+//  NeedleApp
+//
+//  Created by Bof on 05/10/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct CreateDocumentationView: View {
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
