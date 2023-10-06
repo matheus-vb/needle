f7d5672146e6a40c8a0af7f74a3b6d26c83bc9a9
vitoriapinheiro <vps3@cin.ufpe.br>
2023-10-06 16:26:54 -0300

    added code to remove the user but it's missing to delete locally

--- client/NeedleApp/NeedleApp/NeedleApp/Core/Root/RootView.swift
+++ client/NeedleApp/NeedleApp/NeedleApp/Core/Root/RootView.swift
@@ -1,18 +1,14 @@
 //
-//  RootView.swift
-//  NeedleApp
+// RootView.swift
+// NeedleApp
 //
-//  Created by matheusvb on 03/08/23.
+// Created by matheusvb on 03/08/23.
 //
-
 import SwiftUI
-
 struct RootView: View {
-
-    @StateObject var rootViewModel = RootViewModel(manager: AuthenticationManager.shared, notificationDS: NotificationDataService.shared, taskDS: TaskDataService.shared, workspaceDS: WorkspaceDataService.shared)
+    @StateObject var rootViewModel = RootViewModel(manager: AuthenticationManager.shared, notificationDS: NotificationDataService.shared, taskDS: TaskDataService.shared, workspaceDS: WorkspaceDataService.shared, userDS: UserDataService.shared)
+    @State var logout: Bool = false
     @AppStorage("onboard") var isOnboard : Bool = false
-
-    
     init(){
         if (UserDefaults.standard.object(forKey: "onboard") != nil){
             self.isOnboard = false
@@ -21,17 +17,15 @@ struct RootView: View {
             UserDefaults.standard.set(false, forKey: "onboard")
         }
     }
-    
     var body: some View {
         mainView
             .sheet(isPresented: $rootViewModel.showErrorSheet, content: {
                 SheetView(type: .loginError)
-            }) 
+            })
     }
-    
     var mainView: some View {
         ZStack {
-            if rootViewModel.authManager.user == nil {
+            if rootViewModel.authManager.user == nil || logout {
                 if(isOnboard){
                     OnboardingView()
                 } else {
@@ -56,30 +50,55 @@ struct RootView: View {
                                 .font(.custom(SpaceGrotesk.regular.rawValue, size: 14))
                             Image(systemName: rootViewModel.userLogoutIsPresented ? "arrowtriangle.down.fill" : "arrowtriangle.right.fill")
                         }
-                            .onTapGesture{
-                                rootViewModel.userLogoutIsPresented.toggle()
-                                rootViewModel.fetchNotifications()
-                            }
-                            .popover(isPresented: $rootViewModel.userLogoutIsPresented, arrowEdge: .bottom) {
-                                Button {
-                                    rootViewModel.logout()
-                                } label: {
-                                    HStack{
-                                        Text("Sair ")
-                                            .font(.custom(SpaceGrotesk.regular.rawValue, size: 14))
-                                        Image(systemName: "rectangle.portrait.and.arrow.right")
-                                    }
+                        .onTapGesture{
+                            rootViewModel.userLogoutIsPresented.toggle()
+                            rootViewModel.fetchNotifications()
+                        }
+                        .popover(isPresented: $rootViewModel.userLogoutIsPresented, arrowEdge: .bottom) {
+                            VStack{
+                                HStack{
+                                    Text("Sair ")
+                                        .font(.custom(SpaceGrotesk.regular.rawValue, size: 14))
+                                    Image(systemName: "rectangle.portrait.and.arrow.right")
                                 }
-                                .buttonStyle(.plain)
                                 .padding(.horizontal, 20)
                                 .padding(.vertical, 10)
-                                .background(Color.theme.greenMain)
                                 .foregroundColor(Color.theme.blackMain)
+                                .onTapGesture {
+                                    logout.toggle()
+                                    rootViewModel.logout()
+                                    logout.toggle()
+                                }
+//                                Rectangle()
+//                                    .frame(width: 144, height: 1)
+//                                    .foregroundColor(Color.theme.blackMain)
+//                                HStack{
+//                                    Text("Excluir Conta")
+//                                        .font(.custom(SpaceGrotesk.regular.rawValue, size: 14))
+//                                }
+//                                .padding(.horizontal, 20)
+//                                .padding(.vertical, 10)
+//                                .foregroundColor(Color.theme.blackMain)
+//                                .onTapGesture {
+//                                    
+//                                    rootViewModel.deleteAccound()
+//                                    logout.toggle()
+//                                    rootViewModel.logout()
+//                                    logout.toggle()
+//                                }
                             }
-                            .padding(.trailing, 30)
-                            .padding(.leading, 10)
+                        }
+                        .background(.white)
+                        .padding(.trailing, 30)
+                        .padding(.leading, 10)
                     }
             }
         }
     }
 }
+
+
+
+
+
+
